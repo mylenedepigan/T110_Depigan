e5994d1cc23ba72d7b46f5f443d83cdd2a2e77d1
mylenedepigan <73803851+mylenedepigan@users.noreply.github.com>
2021-11-11 09:47:08 +0800

    Create Main_2.java

---
+++ Mi2Act2/package2/Main_2.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.Main;
+public class Main_2{
+		
+		public Main_2(int age) {
+			if(age >=18) {
+				System.out.print("\nYou are Eligible for Voting! Have a great day!\n");
+			}else {
+				System.out.print("\nYou are not eligible for Voting!\n");
+			}
+		}
+}
