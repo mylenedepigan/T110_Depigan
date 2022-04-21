a7aa9758f4f50f49939f04b0e7816088f0ccc219
mylenedepigan <73803851+mylenedepigan@users.noreply.github.com>
2022-04-21 22:55:14 +0800

    Create Ma3FinalTask II-Main_II.java

---
+++ Ma3FinalTask II-Main_II.java
@@ -0,0 +1,12 @@
+public class Main_II {
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		gui.comboBox();
+		gui.buttons();
+		gui.labels();
+		gui.passwordField();
+		gui.textField();
+		gui.radioButtons();
+		gui.frame();
+	}
+}
